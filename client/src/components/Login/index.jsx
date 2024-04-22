b5983e664165208ec497711b352d686ba7b68c1c
AbdulAhad210904 <210904@students.au.edu.pk>
2024-04-22 23:11:16 +0500

    Login & Signup Completed

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 					</form>
 				</div>
 				<div className={styles.right}>
-					<h1>New Here ?</h1>
+					<h1>New User ?</h1>
 					<Link to="/signup">
 						<button type="button" className={styles.white_btn}>
 							Sing Up
